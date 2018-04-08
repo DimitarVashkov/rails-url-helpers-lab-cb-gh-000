c3bac973facbe306609d8a468042226967380250
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-08 14:41:53 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,8 @@ class StudentsController < ApplicationController
   end
 
   def activate
+    @student = Student.find(params[:id])
+    @student.active ? @student.active = false : @student.active = true
   end
 
   private
